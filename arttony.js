7ab68a22bca4e21d269ba6b370b6365c52314e6e
Art Ramirez <artramirez08@gmail.com>
2023-04-18 18:20:03 -0700

    commented rapid api code

---
+++ arttony.js
@@ -0,0 +1,24 @@
+/* Rapid API Movie API
+
+const axios = require("axios");
+
+const options = {
+  method: "GET",
+  url: "https://movie-database-alternative.p.rapidapi.com/",
+  params: { s: "Avengers Endgame", r: "json", page: "1" },
+  headers: {
+    "X-RapidAPI-Key": "9b5643880dmsh28195ce9b2d54dcp127ad7jsnc5dee7a274ef",
+    "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+console.log(axios) */
