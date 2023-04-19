79b44512fce79f1fdb975aa1551c1c6c33226fee
Art Ramirez <artramirez08@gmail.com>
2023-04-18 20:58:20 -0700

    switched request to fetch

--- src/controllers/arttony.js
+++ src/controllers/arttony.js
@@ -1,24 +1,19 @@
 //RapidAPI Movie API
 
-const axios = require("axios");
-
 const options = {
   method: "GET",
-  url: "https://movie-database-alternative.p.rapidapi.com/",
-  params: { s: "Avengers Endgame", r: "json", page: "1" },
   headers: {
     "X-RapidAPI-Key": "9b5643880dmsh28195ce9b2d54dcp127ad7jsnc5dee7a274ef",
     "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
   },
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+fetch(
+  "https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
 module.exports = options
