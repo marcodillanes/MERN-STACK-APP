8e8ce55fd02bca7eed33a5c9d919420e3150f5f8
Art Ramirez <artramirez08@gmail.com>
2023-04-19 22:56:08 -0700

    TODO connect movies controller and server.js

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+//controllers
+const movieController = require('./server/controllers/movies_controller')
+app.use('/api/movies', movieController)
+
 //listen 
 app.listen(4005, () => {
     console.log('Server is running on port 4005')
