163fd14ce3e7ad191adb4ad81ce975767db56716
Andrew Olivares <Wrestler533438@gmail.com>
2023-04-23 10:00:23 -0700

    added react router npm

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useEffect, useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
