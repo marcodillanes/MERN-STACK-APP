b0443973ec26cb12db6bcd61b9d74e363585650d
Andrew Olivares <Wrestler533438@gmail.com>
2023-04-23 11:13:36 -0700

    added seed data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import { useEffect, useState, useRef } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
+  let [data, setData] = useState([])
+
+  useEffect(() => {
+    setData({Title:"Avengers: Endgame",
+    Year:"2019",
+    imdbID:"tt4154796",
+    Type:"movie",
+    Poster:"https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" },{Title:"Marvel Studios' Avengers: Endgame LIVE Red Carpet World Premiere",
+    Year:"2019",
+    imdbID:"tt10240638",
+    Type:"movie",
+    Poster:"https://m.media-amazon.com/images/M/MV5BNThjZDgwZTYtMjdmYy00ZmUyLTk4NTUtMzdjZmExODQ3ZmY4XkEyXkFqcGdeQXVyMjkzMDgyNTg@._V1_SX300.jpg"})
+  },[])
+  console.log(data)
+  
   return (
     <div className="App">
+      <Router>
 
+      </Router>
     </div>
   );
 }
